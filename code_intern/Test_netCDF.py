fb6c71b454d13942d08968386efcb37ee0bec64a
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-19 14:23:52 -0400

    Create Test_netCDF.py

---
+++ code_intern/Test_netCDF.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+from datetime import datetime
+
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
+from netCDF4 import Dataset as cdf4_ds
+from netCDF4 import num2date
+from metpy.units import units
+
+from scipy.ndimage import gaussian_filter
+
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from matplotlib.collections import PatchCollection
+from matplotlib.colors import Normalize
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
+import matplotlib.ticker as ticker
+
+
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+
+if __name__ == '__main__':
+    pr_nc_file = cdf4_ds('/Users/olivier1/Documents/Data_stage/pr_CA_Rockies_3km_P3_ERA5-1h_ISBA_USGS.nc')  # file shape[time, lat, lon]
+    tas_nc_file = cdf4_ds('/Users/olivier1/Documents/Data_stage/tas_CA_Rockies_3km_P3_ERA5-1h_ISBA_USGS.nc')
+
+    times = pr_nc_file.variables['time']
+    vtimes = num2date(times[:], units=times.units)
+    print(f'{vtimes[0]}')
