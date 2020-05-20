fb3a8b1092dc711b61120602adb9104e4bf44d05
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-20 14:36:10 -0400

    Create __init__.py

---
+++ code_intern/__init__.py
@@ -0,0 +1,24 @@
+from netCDF4 import Dataset as cdf4_ds
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+
+import seaborn as sea
+from code_intern import plot_function as plt_int
+
+if __name__ == '__main__':
+    tas_nc_file = cdf4_ds('/Users/olivier1/Documents/Data_stage/tas_CA_Rockies_3km_P3_ERA5-1h_ISBA_USGS.nc')
+    tempe_0=tas_nc_file['tas'][0,:,:]-273.15
+    print(tas_nc_file.variables)
+
+
+    crs_latlon = ccrs.PlateCarree()
+    crs_rotated=ccrs.RotatedPole(pole_latitude=45, pole_longitude=180)
+    latlon=(-117,-114,50,52)
+    # plt_int.pratique(crs_latlon,lonlat)
+    plt_int.make_plot( crs_rotated,latlon,tempe_0)
+    # plt.figure()
+    # plt.title(f"\n Graph d'essaie du plot des températures au temps zéros\n")
+    # ax = sea.heatmap(tempe_0, cmap='coolwarm')
+    # ax.set_xlabel('Longitude [modifie les graduations later] ')
+    # ax.set_ylabel('Latitude [modifie les graduations later]')
+    # plt.show()
