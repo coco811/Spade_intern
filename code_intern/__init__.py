a9f5ebb79bd0cbee08c9c3f24050a400acdc164c
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-23 13:39:23 -0400

    Update __init__.py

--- code_intern/__init__.py
+++ code_intern/__init__.py
@@ -7,7 +7,7 @@ from code_intern import plot_function as plt_int
 
 if __name__ == '__main__':
     tas_nc_file = cdf4_ds('/Users/olivier1/Documents/Data_stage/tas_CA_Rockies_3km_P3_ERA5-1h_ISBA_USGS.nc')
-    print(tas_nc_file.variables)
-    plt_int.plot_graph(tas_nc_file,time=0).plot_temp_heatmap()
+    # print(tas_nc_file['tas'])
+    # plt_int.plot_graph(tas_nc_file).plot_temp_heatmap()
 
 
