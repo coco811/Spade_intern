7058d2ae9beece420a250790b26802c070bc2beb
olivier1 <olivier.chalifour1@ulaval.ca>
2020-05-23 18:28:04 -0400

    Update __init__.py

--- code_intern/__init__.py
+++ code_intern/__init__.py
@@ -8,15 +8,17 @@ try:
     import cPickle as pickle
 except ImportError:  # python 3.x
     import pickle
+
+def get_mean_calc(data):
+    tempe_moy = np.mean(data['tas'][:, :, :], axis=0)
+    with open('Array_mean_temp', 'wb') as fp:
+        pickle.dump(tempe_moy, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
 if __name__ == '__main__':
 
 
     tas_nc_file = cdf4_ds('/Users/olivier1/Documents/Data_stage/tas_CA_Rockies_3km_P3_ERA5-1h_ISBA_USGS.nc')
-    tempe_moy = np.mean(tas_nc_file['tas'][:, :, :], axis=0)
-    with open('Array_mean_temp', 'wb') as fp:
-        pickle.dump(tempe_moy, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
-    # print(tas_nc_file['tas'])
-    # plt_int.plot_graph(tas_nc_file).plot_temp_heatmap()
+    plt_int.plot_graph(tas_nc_file).plot_temp_heatmap()
 
 
